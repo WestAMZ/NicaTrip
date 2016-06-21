e9303e2c487a2026d03e374d4877fe3107f75b0b
ricardo04 <remartinezc1@hotmail.com>
2016-06-21 11:04:16 -0600

    esto son los archivos modificados

--- controllers/php/profileController.php
+++ controllers/php/profileController.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-    include(MODELS_DIR . 'user.php');
-    $user = null;
-    if(isset($_GET['user']))
-    {
-        $user = User::getUserByUserName(Connection::cleanInput($_GET['user']));
-    }
-    else
-    {
-        if(isset($_SESSION['username']))
-        {
-            $user = User::getUserByUserName($_SESSION['username']);   
-        }
-        else
-        {
-            header('Location: ?view=login');
-        }
-    }
-    if($user == null)
-    {
-        Site::getHead('profile not found');
-    }
-    else
-    {
-        Site::getHead($user->getName() .' '. $user->getLastName());
-    }
-    
-    
-    Site::getNav();
-    
-    include(HTML_DIR . 'profile.html');
-?>
-<?php
-
-    Site::getFooter();
-=======
 <?php
     include(MODELS_DIR . 'user.php');
     $user = null;
@@ -65,9 +28,10 @@
 
     Site::getNav();
 
+    include(HTML_DIR . 'profile.html');
+?>
 
+<?php
 
-    Site::getProfile();
     Site::getFooter();
->>>>>>> origin/master
 ?>
