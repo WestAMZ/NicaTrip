198a7eca1d685b70d0e2ce45f32d6e1490557749
ricardo <remartinezc1@hotmail.com>
2016-06-21 11:24:02 -0600

    Update profileController.php

--- controllers/php/profileController.php
+++ controllers/php/profileController.php
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 <?php
     include(MODELS_DIR . 'user.php');
     $user = null;
@@ -34,44 +32,5 @@
     include(HTML_DIR . 'profile.html');
 ?>
 <?php
-
-    Site::getFooter();
-=======
->>>>>>> origin/master
-<?php
-    include(MODELS_DIR . 'user.php');
-    $user = null;
-    if(isset($_GET['user']))
-    {
-        $user = User::getUserByUserName(Connection::cleanInput($_GET['user']));
-    }
-    else
-    {
-        if(isset($_SESSION['username']))
-        {
-            $user = User::getUserByUserName($_SESSION['username']);
-        }
-        else
-        {
-            header('Location: ?view=login');
-        }
-    }
-    if($user == null)
-    {
-        Site::getHead('profile not found');
-    }
-    else
-    {
-        Site::getHead($user->getName() .' '. $user->getLastName());
-    }
-
-
-    Site::getNav();
-
-    include(HTML_DIR . 'profile.html');
-?>
-
-<?php
-
     Site::getFooter();
 ?>
